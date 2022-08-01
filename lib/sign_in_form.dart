86629fb347098413a62b563459564b05189b5002
Resacky <Ruiz-joseph@hotmail.com>
2022-08-01 16:00:27 -0400

    some changes

--- lib/sign_in_form.dart
+++ lib/sign_in_form.dart
@@ -6,7 +6,6 @@ import 'package:google_fonts/google_fonts.dart';
 import "package:http/http.dart" as http;
 
 class SignInForm extends StatefulWidget {
-
   final String lineID;
 
   const SignInForm({Key? key, required this.lineID}) : super(key: key);
@@ -16,7 +15,6 @@ class SignInForm extends StatefulWidget {
 }
 
 class _SignInFormState extends State<SignInForm> {
-
   TextEditingController controller = TextEditingController();
 
   String name = "";
@@ -25,30 +23,33 @@ class _SignInFormState extends State<SignInForm> {
 
   @override
   Widget build(BuildContext context) {
-     return Scaffold(
-         body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-              Text(
-                isNameForm ? "Enter Your Name:" : "Enter Your Phone:",
-                style: GoogleFonts.taviraj(color: const Color(0xFFc05818)), 
-                textScaleFactor: 2.5,
-              ),
-            Center(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  SizedBox(width: 500, 
-                           child: TextField(keyboardType: isNameForm ? TextInputType.number : TextInputType.text,
-                                            controller: controller,
-                                            onSubmitted: (_) => onEnter()
-                                            ,),
-                          ),
-                  ElevatedButton(child: const Icon(Icons.arrow_forward_ios), onPressed: () {
-
-                    onEnter();
-
-                }),
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            isNameForm ? "Enter Your Name:" : "Enter Your Phone:",
+            style: GoogleFonts.taviraj(color: const Color(0xFFc05818)),
+            textScaleFactor: 2.5,
+          ),
+          Center(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                  width: 500,
+                  child: TextField(
+                    keyboardType:
+                        isNameForm ? TextInputType.number : TextInputType.text,
+                    controller: controller,
+                    onSubmitted: (_) => onEnter(),
+                  ),
+                ),
+                ElevatedButton(
+                    child: const Icon(Icons.arrow_forward_ios),
+                    onPressed: () {
+                      onEnter();
+                    }),
               ],
             ),
           ),
@@ -58,44 +59,38 @@ class _SignInFormState extends State<SignInForm> {
   }
 
   onEnter() async {
-
     isNameForm ? name = controller.text : phone = controller.text;
     controller.clear();
 
-      if (isNameForm) {
-
-        setState(() {
+    if (isNameForm) {
+      setState(() {
         isNameForm = false;
-        });
+      });
+    } else {
+      String apiKey = "Aqs2g3xU81u3u9UhObPC9cKT4d4aiMMeT42l8MMs";
 
-      } else {
+      //add code here to send the HTTPS command to speak to the API...
+      final response = await http.post(
+          Uri.parse('https://api.qminder.com/v1/lines/${widget.lineID}/ticket'),
+          // Send authorization headers to the backend.
+          headers: {
+            "X-Qminder-REST-API-KEY": apiKey,
+            'Content-Type': 'application/x-www-form-urlencoded'
+          },
+          body: {
+            'firstName': '$name',
+            'phoneNumber': '$phone'
+          });
 
-        String apiKey = "Aqs2g3xU81u3u9UhObPC9cKT4d4aiMMeT42l8MMs";
+      //potential bugs/ features:
+      // add first name and last name variables...?
+      // make it a mobile app for IOS android and windows...?
+      // have the websites open an internal browser to open the sites, instead of
+      // having the webapp reroute the whole website
+      // program an internal timer to restart the app at a wait of about 2 minutes..?
 
-        //add code here to send the HTTPS command to speak to the API...
-        final response = await http.post(
-        Uri.parse('https://api.qminder.com/v1/lines/${widget.lineID}/ticket'),
-        // Send authorization headers to the backend.
-        headers: {
-        "X-Qminder-REST-API-KEY": apiKey, 
-        'Content-Type': 'application/x-www-form-urlencoded'},
-        body: {'firstName': '$name',
-               'phoneNumber': '$phone'
-              }
-        );
-
-        //potential bugs/ features:
-        // add first name and last name variables...?
-        // make it a mobile app for IOS android and windows...?
-        // have the websites open an internal browser to open the sites, instead of 
-        // having the webapp reroute the whole website
-        // program an internal timer to restart the app at a wait of about 2 minutes..?
-
-        print(response.body);
-        print(response.statusCode);
-
-      }
-
+      print(response.body);
+      print(response.statusCode);
+    }
   }
-
 }
